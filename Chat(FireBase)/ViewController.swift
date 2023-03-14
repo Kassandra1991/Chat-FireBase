d40d448e050b2028f8b8b1b806680f763fb16658
Alex Asichka <sanechka3@mail.ru>
2023-03-14 20:47:21 +0200

    Initial Commit

---
+++ Chat(FireBase)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chat(FireBase)
+//
+//  Created by Aleksandra Asichka on 2023-03-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
