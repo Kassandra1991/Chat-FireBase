c19b494b7aea1ab527d67a44eb0fb06738d51689
Alex Asichka <sanechka3@mail.ru>
2023-03-18 11:44:10 +0200

    create chat vc

--- Chat(FireBase)/Controllers/ChatViewController.swift
+++ Chat(FireBase)/Controllers/ChatViewController.swift
@@ -6,24 +6,29 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ChatViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var messageTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.hidesBackButton = true
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print(signOutError.localizedDescription)
+        }
     }
-    */
-
+    @IBAction func sendButtonPressed(_ sender: UIButton) {
+    }
 }
--- Chat(FireBase)/Controllers/LoginViewController.swift
+++ Chat(FireBase)/Controllers/LoginViewController.swift
@@ -6,10 +6,24 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
 
     @IBOutlet weak var loginTF: UITextField!
     @IBOutlet weak var passwordTF: UITextField!
+    
+    @IBAction func loginPressed(_ sender: UIButton) {
+        guard let email = loginTF.text, let password = passwordTF.text else {
+            return
+        }
+        Auth.auth().signIn(withEmail: email, password: password) {  authRezult, error in
+            if let currentError = error {
+                print(currentError.localizedDescription)
+            } else {
+                self.performSegue(withIdentifier: "LoginToChat", sender: self)
+            }
+        }
+    }
 }
